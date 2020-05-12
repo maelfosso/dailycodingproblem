234ce4b2141d578ad5a344084774947baad01c20
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-05-12 14:10:37 +0100

    537 created with problem description

---
+++ dcp537.py
@@ -0,0 +1,18 @@
+"""
+This is your coding interview problem for today.
+
+This problem was asked by Apple.
+
+A Collatz sequence in mathematics can be defined as follows. Starting with any positive integer:
+
+if n is even, the next number in the sequence is n / 2
+if n is odd, the next number in the sequence is 3n + 1
+It is conjectured that every such sequence eventually reaches the number 1. Test this conjecture.
+
+Bonus: What input n <= 1000000 gives the longest sequence?
+
+We will be sending the solution tomorrow, along with tomorrow's question. As always, feel free to shoot us an email if there's anything we can help with.
+
+Have a great day!
+"""
+
